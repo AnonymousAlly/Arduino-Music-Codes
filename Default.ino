60bf7a2d6c78a57cec3704c97042475c8864d921
AnonymousAlly <45446859+AnonymousAlly@users.noreply.github.com>
2018-11-28 21:17:00 -0800

    Add files via upload

---
+++ Default.ino
@@ -0,0 +1,44 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+tone(3,349,149);//F4 
+delay(149); 
+tone(3,415,149);//Ab/G#4 
+delay(149);
+tone(3,466,149);//Bb/A#4 
+delay(149); 
+tone(3,466,446);//Bb/A#4 
+delay(446);  
+tone(3,415,297);//Ab/G#4 
+delay(1485);
+tone(3,349,149);//F4 
+delay(149); 
+tone(3,415,149);//Ab/G#4 
+delay(149);
+tone(3,466,149);//Bb/A#4 
+delay(149); 
+tone(3,466,446);//Bb/A#4 
+delay(446);  
+tone(3,415,297);//Ab/G#4 
+delay(297);
+tone(3,349,297);//F4 
+delay(297); 
+tone(3,311,149);//Eb/D#4 
+delay(149); 
+tone(3,349,149);//F4 
+delay(595); 
+tone(3,466,149);//Bb/A#4 
+delay(149);
+tone(3,415,149);//Ab/G#4 
+delay(149);
+tone(3,349,149);//F4 
+delay(149); 
+tone(3,311,149);//Eb/D#4 
+delay(149); 
+tone(3,349,149);//F4 
+delay(1931);
+}
